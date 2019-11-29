eb797594d24f480a317e2478a7415146fada1ece
cordotoma_sd2081@CEACCS.NET <cordotoma_sd2081@CEACCS.NET>
2019-11-29 09:40:52 +0800

    It's time to work for back end

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,156 +1,109 @@
 import React, { Component } from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import CardContent from '@material-ui/core/CardContent';
-import Paper from '@material-ui/core/Paper';
-import Card from '@material-ui/core/Card';
+import Avatar from '@material-ui/core/Avatar';
+// import "../Views/App.css";
 
+
+const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+const validateForm = (errors) => {
+  let valid = true;
+  Object.values(errors).forEach(
+    (val) => val.length > 0 && (valid = false)
+  );
+  return valid;
+}
+
+const countErrors = (errors) => {
+  let count = 0;
+  Object.values(errors).forEach(
+    (val) => val.length > 0 && (count = count + 1)
+  );
+  return count;
+}
 export default class Login extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
-      password: "",
-      usernameReqText: "",
-      passwordReqText: "",
-      usernameReqError: false,
-      passwordReqError: false,
-    }
+      formValid: false,
+      errorCount: null,
+      errors: {
+        username: '',
+        password: '',
+      }
+    };
   }
-  checkCredintial = () => {
-    if (this.state.username === "") {
-      this.setState({ usernameReqText: "Required!", usernameReqError: true })
-    }
-    if (this.state.password === "") {
-      this.setState({ passwordReqText: "Required!", passwordReqError: true })
-    } else {
 
+  handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    let errors = this.state.errors;
+
+    switch (name) {
+      case 'username': 
+        errors.username = 
+          validEmailRegex.test(value)
+            ? ''
+            : 'Username must be 5 characters long!';
+        break;
+      case 'password': 
+        errors.password = 
+          value.length < 8
+            ? 'Password must be 8 characters long!'
+            : '';
+        break;
+      default:
+        break;
     }
+
+    this.setState({errors, [name]: value});
   }
-  username = (event) => {
-    this.setState({ username: event.target.value })
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({formValid: validateForm(this.state.errors)});
+    this.setState({errorCount: countErrors(this.state.errors)});
   }
-  password = (event) => {
-    this.setState({password:event.target.value})
-  }
+
   render() {
-    // if (this.state.toCheckout) {
-    //     //go to the Checkout Component
-    //     this.setState({ fnameColor: { color: "black" }, lnameColor: { color: "black" }, emailColor: { color: "black" }, phoneColor: { color: "black" }, paymentColor: { color: "black" } })
-    //     return <Redirect to={{ pathname: "/Confirm", state: { fname: this.state.firstname, lname: this.state.lastname, gmail: this.state.email, cp: this.state.phone, pay: this.state.payment } }} />
-    // }
-    return (
-      <div>
-        {this.loginForm()}
-      </div>
-    )
-  }
-  loginForm() {
+    const {errors, formValid} = this.state;
     const classes = makeStyles(theme => ({
-      body: {
-        backgroundColor: theme.palette.common.white,
-      },
-      paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      },
-      form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-      },
-      submit: {
-        margin: theme.spacing(3, 0, 2),
-      },
       root: {
-        flexGrow: 1,
-      },
-      paper: {
-        height: 100,
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary
+        display: 'flex',
+        '& > *': {
+          margin: theme.spacing(1),
+        },
       },
       bigAvatar: {
-        width: 60,
-        height: 60,
+        width: 80,
+        height: 80,
       },
     }));
-
     return (
-      <Grid container spacing={3} justify="center" style={{ marginTop: '10%' }}>
-        {/* <Grid item xs={12} > */}
-        {/* <Paper className={classes.paper} > */}
-        {/* <Grid container justify='space-around' style={{ height: '50%', marginTop: '8%' }}> */}
-        <Grid>
-          <Card style={{ height: '500px', width: '400px' }}>
-            <CardContent >
-              <CssBaseline />
-              {/* <div className={classes.paper}> */}
-              <center>
-                <Avatar style={{ height: '120px', width: '130px' }} alt="Remy Sharp" src="https://cdn1.vectorstock.com/i/1000x1000/11/10/admin-icon-male-person-profile-avatar-with-gear-vector-25811110.jpg" className={classes.bigAvatar} />
-                <Typography component="h4" variant="h4">
-                  Admin
-                      </Typography></center>
-              <Typography></Typography>
-              <form className={classes.form} noValidate>
-                <TextField
-                  error={this.state.usernameReqError}
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="username"
-                  label="Username"
-                  name="username"
-                  helperText={this.state.usernameReqText}
-                  onChange={this.username}
-                  autoComplete="username"
-                  autoFocus
-                />
-                <TextField
-                  error={this.state.passwordReqError}
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  helperText={this.state.passwordReqText}
-                  autoComplete="current-password"
-                />
-                <Button
-                  // type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  style={{ marginTop: "50px" }}
-                  className={classes.submit}
-                  onClick={this.checkCredintial}
-                >
-                  Login
-                    </Button>
-              </form>
-              {/* </div> */}
-            </CardContent>
-          </Card>
-        </Grid>
-        {/* </Grid> */}
-        {/* </Paper> */}
-        {/* </Grid> */}
-      </Grid>
-
+      <div className='wrapper'>
+        <div className='form-wrapper'>
+        <center><Avatar alt="Remy Sharp" src="https://cdn1.vectorstock.com/i/1000x1000/11/10/admin-icon-male-person-profile-avatar-with-gear-vector-25811110.jpg" style={{height:'130px',width: '140px',}}className={classes.bigAvatar} />
+          <h2>Admin</h2></center>
+          <form onSubmit={this.handleSubmit} noValidate>
+          <div className='username'>
+              <label htmlFor="username">Username</label>
+              <input type='username' name='username' onChange={this.handleChange} noValidate />
+              {errors.username.length > 0 && 
+                <span className='error'>{errors.username}</span>}
+            </div>
+            <div className='password'>
+              <label htmlFor="password">Password</label>
+              <input type='password' name='password' onChange={this.handleChange} noValidate />
+              {errors.password.length > 0 && 
+                <span className='error'>{errors.password}</span>}
+            </div>
+            <div className='submit'>
+              <button>submit</button>
+            </div>
+            <div className= "info">{this.state.errorCount !== null ? <p className="form-status">Form is {formValid ? 'valid ✅' : 'invalid ❌'}</p> : ''}</div>
+          </form>
+        </div>
+      </div>
     );
   }
 }
